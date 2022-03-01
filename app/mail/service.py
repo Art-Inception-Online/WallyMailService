20a2fec26db6a34dd85f3ecb0739e054666eb4bb
GR <admin@admin.ge>
2022-03-01 15:21:33 +0400

    new: _TABLE_SENT

--- app/mail/service.py
+++ app/mail/service.py
@@ -4,6 +4,7 @@ from app.helpers.db import Database
 class Email:
     _TABLE_EMAILS = '_emails'
     _TABLE_DOMAINS = '_domains'
+    _TABLE_SENT = '_sent_emails'
 
     def __init__(self):
         self._db = Database()
