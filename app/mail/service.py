c4b218115d9d0a0548eac87d8b7a4aefc607a296
GR <admin@admin.ge>
2022-03-06 03:52:09 +0400

    new: '_emails_verification_log' table

--- app/mail/service.py
+++ app/mail/service.py
@@ -6,6 +6,7 @@ class Email:
     _TABLE_DOMAINS = '_domains'
     _TABLE_SENT = '_sent_emails'
     _TABLE_WEBHOOK_EVENTS = '_sent_email_events'
+    _TABLE_VERIFICATION_LOG = '_emails_verification_log'
 
     def __init__(self):
         self._db = Database()
