aab3e8210fbef63dc8a14d48a91a7bfdcffc4458
GR <admin@admin.ge>
2022-03-08 03:59:24 +0400

    new: list classmethod

--- app/utils/enum_extended.py
+++ app/utils/enum_extended.py
@@ -5,3 +5,7 @@ class EnumExtended(Enum):
     @classmethod
     def has_value(cls, value):
         return value in cls._value2member_map_
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
